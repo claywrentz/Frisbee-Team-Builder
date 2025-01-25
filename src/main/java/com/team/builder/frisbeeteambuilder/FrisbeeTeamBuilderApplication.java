78f8487caf199a1d4a75c969a3d03a2e06c5c2a1
claywrentz <cwrentz@smu.edu>
2025-01-25 12:38:16 -0600

    Initial commit

---
+++ src/main/java/com/team/builder/frisbeeteambuilder/FrisbeeTeamBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.team.builder.frisbeeteambuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrisbeeTeamBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrisbeeTeamBuilderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team/builder/frisbeeteambuilder/FrisbeeTeamBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team.builder.frisbeeteambuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrisbeeTeamBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
