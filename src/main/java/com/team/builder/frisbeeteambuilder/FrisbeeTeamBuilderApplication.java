ed4ff37d2faf43729baddfec6d96f63bfa2ee90e
claywrentz <cwrentz@smu.edu>
2025-01-27 15:38:35 -0600

    Setting up springboot + H2 with jpa

--- src/main/java/com/team/builder/frisbeeteambuilder/FrisbeeTeamBuilderApplication.java
+++ src/main/java/com/team/builder/frisbeeteambuilder/FrisbeeTeamBuilderApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FrisbeeTeamBuilderApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FrisbeeTeamBuilderApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FrisbeeTeamBuilderApplication.class, args);
+    }
 
 }
--- src/test/java/com/team/builder/frisbeeteambuilder/FrisbeeTeamBuilderApplicationTests.java
+++ src/test/java/com/team/builder/frisbeeteambuilder/FrisbeeTeamBuilderApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class FrisbeeTeamBuilderApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
